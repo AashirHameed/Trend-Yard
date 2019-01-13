de27eecc0e106cdb07b2c1e0f1cd8ba7ad466f79
aashi <aashi@DESKTOP-HKHOQEL>
2019-01-14 00:47:29 +0500

    Test

---
+++ Trend Yard/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace Trend_Yard
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ Trend Yard/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Trend_Yard.MvcApplication" Language="C#" %>
